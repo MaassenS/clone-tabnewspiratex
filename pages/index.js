fb337efc36c0a79aeaa6b473beffd93746abfd72
Otávio Maassen Schweinitz <135477731+MaassenS@users.noreply.github.com>
2026-01-01 22:54:02 +0000

    chore: configura .gitignore e adiciona .nvmrc

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 function Home() {
-  return <h1>Olá, mundo! Atualiza automaticamente</h1>;
+  return <h1>Site presentation</h1>;
+         <h2>Adding a new commit!</h2>
 }
 
 export default Home;
-// ↑ export default é ESSENCIAL - Next precisa saber qual componente renderizar
+// ↑ export default é ESSENCIAL - Next precisa saber qual componente renderiza 
