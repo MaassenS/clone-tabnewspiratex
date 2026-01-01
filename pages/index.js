3772dae3856bdb7cac1f58162ab72351398eeb05
Otávio Maassen Schweinitz <135477731+MaassenS@users.noreply.github.com>
2026-01-01 22:42:52 +0000

    feat: adiciona estrutura inicial do projeto Next.js

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() {
+  return <h1>Olá, mundo! Atualiza automaticamente</h1>;
+}
+
+export default Home;
+// ↑ export default é ESSENCIAL - Next precisa saber qual componente renderizar
