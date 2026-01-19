5b698da1aa0f0c7609a3dade593128163c13aeea
MaassenS <tatamaassen@gmail.com>
2026-01-19 17:09:21 -0300

    add scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 function Home() {
-  return <><h1>Site presentation</h1><h2>Adding a new commit!</h2></>;
+  return (
+    <>
+      <h1>Site presentation</h1>
+      <h2>Adding a new commit!</h2>
+    </>
+  );
 }
 
 export default Home;
-// ↑ export default é ESSENCIAL - Next precisa saber qual componente renderiza 
+// ↑ export default é ESSENCIAL - Next precisa saber qual componente renderiza
